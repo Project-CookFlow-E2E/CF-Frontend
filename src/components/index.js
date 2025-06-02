b4f16b4e4aa605fb0098d8d51d9486780f024bd6
srlsrx <nicolasin97@gmail.com>
2025-06-02 10:00:58 +0200

    feat: add index file to centralize component exports

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import Boton from './Boton';
+import Card from './Card';
+import CheckboxCategoria from './CheckboxCategoria';
+import CheckboxFiltro from './CheckboxFiltro';
+import Footer from './Footer';
+import Header from './Header';
+import Imagen from './Imagen';
+import Input from './Input';
+
