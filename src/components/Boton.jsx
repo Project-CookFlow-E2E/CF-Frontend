925e842d324e77eb573fbbf60e8a2daa53d58075
sergio-jorquera <sjorkera@gmail.com>
2025-06-02 13:13:30 +0200

    refactor: add styles botton to className

--- src/components/Boton.jsx
+++ src/components/Boton.jsx
@@ -1,18 +1,15 @@
 import React from "react";
 
 const Boton = ({ children, onClick, type = "button", className = "", disabled = false }) => {
-  const baseStyles = `
-    px-4 py-2 rounded-xl font-medium transition-colors duration-300
-    bg-gray-200 text-gray-800 hover:bg-gray-300
-    disabled:bg-gray-100 disabled:text-gray-400 disabled:cursor-not-allowed
-  `;
-
   return (
     <button
       type={type}
       onClick={onClick}
       disabled={disabled}
-      className={`${baseStyles} ${className}`}
+      className={`px-4 py-2 rounded-xl font-medium transition-colors duration-300
+                  bg-accent text-gray-800 hover:bg-gray-300
+                  disabled:bg-gray-100 disabled:text-gray-400 disabled:cursor-not-allowed
+                  ${className}`}
     >
       {children}
     </button>
