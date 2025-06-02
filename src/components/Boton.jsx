7a2deaecdc9fe163220d6c813a6fa5e294ec64a4
sergio-jorquera <sjorkera@gmail.com>
2025-06-02 15:28:46 +0200

    refactor:deleted component Button homepage

--- src/components/Boton.jsx
+++ src/components/Boton.jsx
@@ -7,7 +7,7 @@ const Boton = ({ children, onClick, type = "button", className = "", disabled = 
       onClick={onClick}
       disabled={disabled}
       className={`px-4 py-2 rounded-xl font-medium transition-colors duration-300
-                  bg-accent text-gray-800 hover:bg-gray-300
+                  bg-accent text-white hover:bg-gray-300
                   disabled:bg-gray-100 disabled:text-gray-400 disabled:cursor-not-allowed
                   ${className}`}
     >
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,9 +5,7 @@ const Home = () => {
   return (
     <div className="p-4">
       <div>Home</div>
-      <Boton onClick={() => alert("Click en el botón")}>
-        ¡Presióname!
-      </Boton>
+      
     </div>
   )
 }
