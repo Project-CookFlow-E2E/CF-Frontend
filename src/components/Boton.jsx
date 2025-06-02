709f1617ab903648840d674e4e9b2e78993dd57d
sergio-jorquera <sjorkera@gmail.com>
2025-06-02 12:12:43 +0200

    feat(components): crear componente Boton reutilizable

--- src/components/Boton.jsx
+++ src/components/Boton.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React from "react";
 
-const Boton= () => {
+const Boton = ({ children, onClick, type = "button", className = "", disabled = false }) => {
+  const baseStyles = `
+    px-4 py-2 rounded-xl font-medium transition-colors duration-300
+    bg-gray-200 text-gray-800 hover:bg-gray-300
+    disabled:bg-gray-100 disabled:text-gray-400 disabled:cursor-not-allowed
+  `;
+
   return (
-    <div>Este es mi componente Boton.</div>
+    <button
+      type={type}
+      onClick={onClick}
+      disabled={disabled}
+      className={`${baseStyles} ${className}`}
+    >
+      {children}
+    </button>
+  );
+};
 
-  )
-}
-
-export default Boton
+export default Boton;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import {Boton} from '../components'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="p-4">
+      <div>Home</div>
+      <Boton onClick={() => alert("Click en el botón")}>
+        ¡Presióname!
+      </Boton>
+    </div>
   )
 }
 
