b24b7dc797845711e5f9c82116d3b3c98691de98
julia-martynovych <inquam414@gmail.com>
2025-06-03 11:42:34 +0200

    fix: clear comments at root

--- src/App.jsx
+++ src/App.jsx
@@ -10,16 +10,16 @@ function App() {
     <BrowserRouter>
     <Header/>
       <Routes>
-        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        {/* <Route path="/signin" element={<SignIn />} />
+        <Route path="/signin" element={<SignIn />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/recipe/:id" element={<Recipe />} />
         <Route path="/search" element={<Search />} />
         <Route path="/inspire-me" element={<InspireMe />} />
         <Route path="/add-recipe" element={<AddRecipe />} />
         <Route path="/admin-dashboard" element={<AdminDashboard />} />
-        <Route path="/shopping-list" element={<ShoppingList />} /> */}
+        <Route path="/shopping-list" element={<ShoppingList />} />
       </Routes>
       <Footer/>
     </BrowserRouter>
