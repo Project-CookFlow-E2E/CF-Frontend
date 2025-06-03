28982af69e4444a65156c701174298d4e94559c3
hema <hemapriyaweb@gmail.com>
2025-06-03 13:29:38 +0200

    Added RecipeIngredientsChecklist component

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -1,6 +1,10 @@
-const Recipe = () => {
-  return <div></div>;
-};
+import RecipeIngredientsChecklist from '../components/RecipeIngredientsChecklist';
 
-export default Recipe;
-
+export default function Recipe() {
+  return (
+    <div className="max-w-4xl mx-auto p-4">
+      {/* Ingredients checklist */}
+      <RecipeIngredientsChecklist />
+    </div>
+  );
+}
