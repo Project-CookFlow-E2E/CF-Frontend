201bed885c0e9cf2de45d32664a55bc3ea790ff7
srlsrx <nicolasin97@gmail.com>
2025-06-03 14:37:00 +0200

    feat: add page logic to the Add Recipe view

---
+++ src/data/mockData.js
@@ -0,0 +1,16 @@
+export const categoriasMock = [
+  "Desayuno",
+  "Almuerzo",
+  "Cena",
+  "Merienda",
+  "Postre",
+];
+export const ingredientesMock = [
+  "Huevos",
+  "Leche",
+  "Harina",
+  "Azúcar",
+  "Mantequilla",
+  "Plátano",
+  "Chocolate",
+];
