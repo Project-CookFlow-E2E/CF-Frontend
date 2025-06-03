5b6db5281e7a2872f4240aa2f1d861939d325052
sarayminar <sarayminar@gmail.com>
2025-06-03 09:21:00 +0200

    feat: creating login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Input from "../../components/Input"
+import Boton from "../../components/Boton"
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div>
+      <h2>Hello again!</h2>
+      <h4>Enter your details to continue</h4>
+    </div>
   )
 }
 
